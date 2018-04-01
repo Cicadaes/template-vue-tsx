7ccef8a916b1ddd495a161b001d250928bd70dd6
Cicadaes <cicadaes@sina.com>
2018-03-31 20:12:36 -0700

    tslint has no initializer and is not assigned directly in the constructor

--- src/component/HelloDecorator.vue
+++ src/component/HelloDecorator.vue
@@ -6,27 +6,7 @@
   </div>
 </template>
   
-<script lang='ts'>
-  import { Vue, Component, Prop } from 'vue-property-decorator'
-
-  @Component
-  export default class HelloDecorator extends Vue {
-    @Prop() name!: string
-    @Prop() initialEnthusiasm!: number
-
-    enthusiasm= this.initialEnthusiasm
-
-    increment() { this.enthusiasm++; }
-    decrement() {
-      if (this.enthusiasm > 1) {
-        this.enthusiasm--;
-      }
-    }
-    get exclamationMarks(): string {
-        return Array(this.enthusiasm + 1).join('!');
-      }
-  }
-</script>
+<script lang='ts' src='./HelloDecorator'></script>
 
 <style lang="stylus">
   .greeting
