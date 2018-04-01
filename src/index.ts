4765f48df6d64ae9debcb5333d6207bf4c7ed55e
Cicadaes <cicadaes@sina.com>
2018-03-31 18:45:14 -0700

    Using decorators to define a component

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 import Vue from 'vue'
-import HelloComponent from './component/Hello'
+import HelloComponent from './component/Hello.vue'
+import HelloDecoratorComponent from './component/HelloDecorator.vue'
 
 new Vue({
   el: '#root',
   components: {
-    HelloComponent
+    HelloComponent,
+    HelloDecoratorComponent
   },
   template:
   `
     <div>
       <div>Hello {{ name }}!</div>
       Name : <input v-model='name' type='text'>
+      <h1>Hello Component</h1>
       <hello-component :name="name" :initialEnthusiasm="5" />
+      <h1>Hello Decorator Component</h1>
+      <hello-decorator-component :name="name" :initialEnthusiasm="5" />
     </div>
   `,
   data: {
